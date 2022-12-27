c71fafc92826fa391e328eaf7f357e3811145438
Robin Aerts <nibor.aerts@gmail.com>
2022-12-27 20:23:25 +0100

    added vercel redirect

--- next.config.js
+++ next.config.js
@@ -4,16 +4,6 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/make",
-        destination:
-          "https://raw.githubusercontent.com/robinaerts/makeproject/main/makeproject.ps1",
-        statusCode: 200,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
